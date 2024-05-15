ced21ea27d491c3fc708112182d527e2ced0d734
Sasha Syniak <sashasyniak04@gmail.com>
2024-05-15 14:17:00 +0300

    database setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,41 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import User from "./models/film";
 
 const PORT = process.env.PORT || 7010;
+
+console.log(process.env.MONGODB_CONNECTION_STRING);
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database!"));
+
+  getUsers();
+async function getUsers() {
+  try {
+    const users = await User.find();
+    console.log("Users", users);
+  } catch (e) {
+    console.log(e);
+  }
+}
+// addUser();
+async function addUser() {
+  try {
+    const user = new User({
+      email: "lyashko@gmail.com",
+      firstname: "Kyle",
+      lastname: "Lyashka",
+      age: 26,
+    });
+    await user.save();
+    console.log("Users", user);
+  } catch (e) {
+    console.log(e);
+  }
+}
 
 const app = express();
 app.use(express.json());
