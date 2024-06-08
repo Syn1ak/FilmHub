b8dcaa321417b39f0fa225e5197caf20922860ee
Striletska Kateryna <striletskay2005@gmail.com>
2024-06-08 19:59:39 +0300

    Fixed bug

--- backend/src/controllers/SessionController.ts
+++ backend/src/controllers/SessionController.ts
@@ -1,62 +1,60 @@
-import { Request, Response } from "express";
+import {Request, Response} from "express";
 import Session from "../models/session";
 
 const getSessionByMovieAndDate = async (req: Request, res: Response) => {
-  const { movie_id, cinema_id, date } = req.query;
-  if( !movie_id || !cinema_id || !date ){
-    return res.status(401).json({message: "Cannot find some required field!"});
-  }
-
-  try {
-    const dateString = Array.isArray(date) ? date[0] : date;
-
-    if (typeof dateString !== 'string') {
-      return res.status(400).json({ message: "Invalid date format" });
+    const {movie_id, cinema_id, date} = req.query;
+    if (!movie_id || !cinema_id || !date) {
+        return res.status(401).json({message: "Cannot find some required field!"});
     }
-    const convertedDate = new Date(dateString);
-    const today = new Date();
-    console.log(today);
-    console.log(convertedDate)
-    console.log(date)
-    if(convertedDate < today)
-    return res.status(200).json([]);
-    else {
-      const sessionByMovieAndDate = await Session.find({
-        movie: movie_id,
-        $expr: {
-          $eq: [
-            date,
+
+    try {
+        const dateString = Array.isArray(date) ? date[0] : date;
+
+        if (typeof dateString !== 'string') {
+            return res.status(400).json({message: "Invalid date format"});
+        }
+        const today = new Date();
+        today.setHours(today.getHours() -9);
+        console.log("T", today);
+
+        const sessionByMovieAndDate = await Session.find({
+            movie: movie_id,
+            $expr: {
+                $eq: [
+                    date,
+                    {
+                        $dateToString: {date: "$start_time", format: "%Y-%m-%d"},
+                    },
+                ],
+            },
+        }).populate([
             {
-              $dateToString: {date: "$start_time", format: "%Y-%m-%d"},
+                path: "hall",
+                match: {cinema_id: cinema_id},
+                populate: {
+                    path: "cinema_id",
+                    populate: {
+                        path: "city_id",
+                    },
+                },
             },
-          ],
-        },
-      }).populate([
-        {
-          path: "hall",
-          match: {cinema_id: cinema_id},
-          populate: {
-            path: "cinema_id",
-            populate: {
-              path: "city_id",
+            {
+                path: "movie",
             },
-          },
-        },
-        {
-          path: "movie",
-        },
-      ]);
+        ]);
 
-      const sessions = sessionByMovieAndDate.filter((item) => item.hall != null);
-      console.log(sessions.length)
-      res.json(sessions);
+        const sessions = sessionByMovieAndDate.filter((item) => {
+            if (item.hall != null && item.start_time > today)
+                return item;
+        });
+        console.log(sessions.length)
+        res.json(sessions);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({message: "Something went wrong"});
     }
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ message: "Something went wrong" });
-  }
 };
 
 export default {
-  getSessionByMovieAndDate,
+    getSessionByMovieAndDate,
 };
